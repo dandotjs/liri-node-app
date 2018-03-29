b1f7d0a96f5505858cb4ed636ace9836971cb273
oxygenplug <dan.garces2@gmail.com>
2018-03-29 18:18:01 -0400

    env magically disappeared last night so readded the env file

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ var client = new twitter(apiKeys.twitter);
 
 var spotify = new spotify(apiKeys.spotify);
 
-client.stream('statuses/filter', {track: 'twitter'},  function(stream) {
-    stream.on('data', function(tweet) {
-      console.log(tweet.text);
-    });
-  
-    stream.on('error', function(error) {
-      console.log(error);
-    });
-  });
+
+$.ajax({
+    url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + '' + '&count=20'
+})
