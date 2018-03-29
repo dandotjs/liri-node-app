a60bf93d8a19a2f253f6abb567f0925049b41e5a
oxygenplug <dan.garces2@gmail.com>
2018-03-29 17:35:20 -0400

    configured api keys for twitter and spotify

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-
 require("dotenv").config();
 
-const apiKeys = require("keys.js")
+const apiKeys = require("keys.js");
 
-var client = new twitter(keys.twitter);
+var client = new twitter(apiKeys.twitter);
 
-var spotify = new spotify(keys.spotify);
+var spotify = new spotify(apiKeys.spotify);
+
+client.stream('statuses/filter', {track: 'twitter'},  function(stream) {
+    stream.on('data', function(tweet) {
+      console.log(tweet.text);
+    });
+  
+    stream.on('error', function(error) {
+      console.log(error);
+    });
+  });
