430d099823981291192e8f98e388b58ab5a54279
oxygenplug <dan.garces2@gmail.com>
2018-04-01 22:34:26 -0400

    twitter search now functional

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 require("dotenv").config();
 
-const apiKeys = require("keys.js");
+const request = require('request');
 
-var client = new Twitter(apiKeys.twitter);
+const apiKeys = require("./keys.js");
 
-var spotify = new Spotify(apiKeys.spotify);
+const Twitter = require('twitter');
+
+const inquirer = require('inquirer');
 
 
-$.ajax({
-    method: GET,
-    url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + client.consumer_key + '&count=20',
-})
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const spotify = apiKeys.spotify;
+
+let twitterUsername = ""
+
+/*;*/
+
+
+function runTwitter() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter your twitter username',
+            name: 'twitterUsername',
+        }
+    ])
+        .then(answers => {
+            twitterUsername = (answers);
+            console.log('Here are ' + twitterUsername.twitterUsername + '\'s\ ' +  'twenty latest tweets \n');
+
+
+
+            client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + twitterUsername.twitterUsername + '&count=20', function (error, tweets, response) {
+                if (error) throw error;
+
+                for (i = 0; i < tweets.length; i++) {
+                    console.log('- ' + tweets[i].text + '\n');
+                    // console.log(JSON.stringify(tweets, null, 2));
+                }
+
+            });
+        })
+}
+
+runTwitter();
